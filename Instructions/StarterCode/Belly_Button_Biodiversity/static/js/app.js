24b50b8170b1b9bac27ca9046af0dfe5cdb0b47e
lisatian8 <lt8760@yahoo.com>
2019-08-16 21:53:45 -0400

    Instructions/

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -54,3 +54,20 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
+
+// test
+//Part 5 - Working Pie Chart
+ var trace1 = {
+   labels: ["beer", "wine", "martini", "margarita",
+      "ice tea", "rum & coke", "mai tai", "gin & tonic"],
+   values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+   type: 'pie'
+};
+
+var data = [trace1];
+
+ var layout = {
+   title: "'Pie' Chart",
+ };
+
+ Plotly.newPlot("pie", data, layout);
