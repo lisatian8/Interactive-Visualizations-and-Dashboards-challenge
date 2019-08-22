d574e02dea9812ae48145ae597d314892bf310c0
lisatian8 <lt8760@yahoo.com>
2019-08-21 21:00:24 -0400

    Instructions/

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,7 +4,7 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    
     // Use `.html("") to clear any existing metadata
 
     // Use `Object.entries` to add each key and value pair to the panel
@@ -13,6 +13,22 @@ function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+
+    //add code below
+
+    d3.json(/metadata/${sample}).then((data) => {
+      // Use d3 to select the panel with id of #sample-metadata
+      var PANEL = d3.select("#sample-metadata");
+      // Use `.html("") to clear any existing metadata
+      PANEL.html("");
+      // Use Object.entries to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+      Object.entries(data).forEach(([key, value]) => {
+        PANEL.append("h6").text(${key}: ${value});
+      });
+      // BONUS: Build the Gauge Chart
+      buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
@@ -24,6 +40,29 @@ function buildCharts(sample) {
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+    d3.json(/samples/${sample}).then((data) => {
+      const otu_ids = data.otu_ids;
+      const otu_labels = data.otu_labels;
+      const sample_values = data.sample_values;
+      // Build a Bubble Chart
+      var bubbleLayout = {
+        margin: { t: 0 },
+        hovermode: "closest",
+        xaxis: { title: "OTU ID" }
+      };
+      var bubbleData = [
+        {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          }
+        }
 }
 
 function init() {
@@ -38,6 +77,8 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
+    console.log("sampleNames");
+    console.log(sampleNames);
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
