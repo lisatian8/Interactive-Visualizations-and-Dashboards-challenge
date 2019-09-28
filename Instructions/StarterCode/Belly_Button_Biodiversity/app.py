a217dfd4fcc5b30c39415dd7fe1047dcd60ebbd4
lisatian8 <lt8760@yahoo.com>
2019-09-28 05:36:24 -0400

    Instructions/

--- Instructions/StarterCode/Belly_Button_Biodiversity/app.py
+++ Instructions/StarterCode/Belly_Button_Biodiversity/app.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 import numpy as np
 
-import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
